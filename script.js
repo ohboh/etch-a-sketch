465eb95658de37f9388430d2668e7dfda9639073
ohboh <depafoboe@gmail.com>
2022-11-01 18:25:38 +0800

    Add adjustable range slider

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var tilesetSize = 24 * 16;
-    for (let i = 0; i < tilesetSize; i++){
-        var div = document.createElement("div");
-        div.className = "tile";
-        document.getElementById("grid-container").appendChild(div);
-    }
-}, false)
+
 
 function hoverShade(e){
     if (e.target.className == "tile"){
@@ -14,5 +7,33 @@ function hoverShade(e){
 }
 
 
-const gridContainer = document.querySelector("#grid-container");
+const gridContainer = document.getElementById("grid-container");
 gridContainer.addEventListener("mouseover", hoverShade);
+
+var slider = document.getElementById("myRange");
+var denominatorHTML = document.getElementById("denominator");
+var numeratorHTML = document.getElementById("numerator");
+var denominatorValue = slider.value;
+var numeratorValue = (denominatorValue * 1.5);
+
+denominatorHTML.innerHTML = denominatorValue;
+numeratorHTML.innerHTML = numeratorValue;
+
+slider.oninput = function() {
+    var denominatorValue = this.value;
+    var numeratorValue = (denominatorValue * 1.5);
+    denominatorHTML.innerHTML = denominatorValue;
+    numeratorHTML.innerHTML = numeratorValue;
+    var tilesetSize = denominatorValue * numeratorValue;
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    gridContainer.style.gridTemplateRows = "repeat(" + denominatorValue + ", 1fr)";
+    gridContainer.style.gridTemplateColumns = "repeat(" + numeratorValue + ", 1fr)"
+
+    for (let i = 0; i < tilesetSize; i++){
+        var div = document.createElement("div");
+        div.className = "tile";
+        gridContainer.appendChild(div);
+    }
+}
